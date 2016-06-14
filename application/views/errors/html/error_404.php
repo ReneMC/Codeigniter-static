aa1095e78992ea7727dc987c789333a5c6cb0aa0
ReneMC <rene.mejiac@gmail.com>
2016-06-14 10:52:05 -0700

    + Error 404 custom page

--- application/views/errors/html/error_404.php
+++ application/views/errors/html/error_404.php
@@ -1,64 +1,105 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<title>404 Page Not Found</title>
 <style type="text/css">
+  body {
+    background-color: #342643;
+    color: #fff;
+    font: 100% "Lato", sans-serif;
+    font-size: 1.8rem;
+    font-weight: 300;
+  }
 
-::selection { background-color: #E13300; color: white; }
-::-moz-selection { background-color: #E13300; color: white; }
+  a {
+    color: #75C6D9;
+    text-decoration: none;
+  }
 
-body {
-	background-color: #fff;
-	margin: 40px;
-	font: 13px/20px normal Helvetica, Arial, sans-serif;
-	color: #4F5155;
-}
+  h3 {
+    margin-bottom: 1%;
+  }
 
-a {
-	color: #003399;
-	background-color: transparent;
-	font-weight: normal;
-}
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    line-height: 50px;
+  }
 
-h1 {
-	color: #444;
-	background-color: transparent;
-	border-bottom: 1px solid #D0D0D0;
-	font-size: 19px;
-	font-weight: normal;
-	margin: 0 0 14px 0;
-	padding: 14px 15px 10px 15px;
-}
+  li a:hover {
+    color: #fff;
+  }
 
-code {
-	font-family: Consolas, Monaco, Courier New, Courier, monospace;
-	font-size: 12px;
-	background-color: #f9f9f9;
-	border: 1px solid #D0D0D0;
-	color: #002166;
-	display: block;
-	margin: 14px 0 14px 0;
-	padding: 12px 10px 12px 10px;
-}
+  .center {
+    text-align: center;
+  }
 
-#container {
-	margin: 10px;
-	border: 1px solid #D0D0D0;
-	box-shadow: 0 0 8px #D0D0D0;
-}
+  /* Search Bar Styling */
+  form > * {
+    vertical-align: middle;
+  }
 
-p {
-	margin: 12px 15px 12px 15px;
-}
+  .srchBtn {
+    border: 0;
+    border-radius: 7px;
+    padding: 0 17px;
+    background: #e74c3c;
+    width: 160px;
+    border-bottom: 5px solid #c0392b;
+    color: #fff;
+    height: 65px;
+    font-size: 1.5rem;
+    cursor: pointer;
+  }
+
+  .srchBtn:active {
+    border-bottom: 0px solid #c0392b;
+  }
+
+  .srchBtn:focus {
+    outline: 0;
+  }
+
+  .srchFld {
+    border: 0;
+    border-radius: 7px;
+    padding: 0 17px;
+    max-width: 404px;
+    width: 40%;
+    border-bottom: 5px solid #bdc3c7;
+    height: 60px;
+    color: #7f8c8d;
+    font-size: 19px;
+  }
+
+  .srchFld:focus {
+    outline-color: rgba(255, 255, 255, 0);
+  }
+
+  /* 404 Styling */
+  .header {
+    font-size: 13rem;
+    font-weight: 700;
+    margin: 2% 0 2% 0;
+    text-shadow: 0px 3px 0px #7f8c8d;
+  }
+
+  /* Error Styling */
+  .error {
+    margin: -70px 0 2% 0;
+    font-size: 7.4rem;
+    text-shadow: 0px 3px 0px #7f8c8d;
+    font-weight: 100;
+  }
+
 </style>
-</head>
-<body>
-	<div id="container">
-		<h1>Oops! <?php echo $heading; ?></h1>
-		<?php echo $message; ?>
-	</div>
-</body>
-</html>
+
+
+<section class="center">
+  <article>
+    <h1 class="header">404</h1>
+    <p class="error">ERROR</p>
+  </article>
+  <article>
+    <p>We're sorry, the page you're looking for doesn't exist.</p>
+    <button onclick="window.location.href='home'" class="srchBtn">Go back</button>
+  </article>
+
+</section>
